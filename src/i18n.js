bccfa61864b9a06bdf331aa018e81a697c2eb521
kevin.chou <kevin.chou@wabow.com>
2025-12-23 09:39:55 +0800

    install react-i18next

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n.use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        supportedLngs: ['en', 'zh'],
+        defaultNS: 'common',
+        backend: {
+            loadPath: process.env.REACT_APP_PUBLIC_URL + '/locales/{{lng}}/{{ns}}.json'
+        },
+        debug: true,
+        detection: {
+            order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+            caches: ['cookie']
+        },
+        interpolation: {
+            escapeValue: false
+        }
+    })
+
+export default i18n
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import './i18n';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
@@ -12,13 +13,15 @@ let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Suspense fallback="loading">
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
